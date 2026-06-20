04d709528ab094944610d55914d350ce7b5723ec
sabores-del-mundo <congreso775@gmail.com>
2026-06-19 19:08:45 -0600

    Make DOMContentLoaded listener async and fetch data
    Updated event listener to be asynchronous and added fetch logic to retrieve participant data based on 'matriculaPrueba'.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+
     const nombre = document.getElementById("nombre");
 
-    // Ejemplo de prueba
-    nombre.textContent = "Paola Vélez";
+    const matriculaPrueba = "126381237";
+
+    try {
+
+        const respuesta = await fetch(
+            "https://script.google.com/macros/s/AKfycbwcQDGyo0rVFOd1Yt9fYQhwHMOpidgjKP2j67gNR3lbvyv-gCALKrhrxBgDTK214gn6_g/exec",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    matricula: matriculaPrueba
+                })
+            }
+        );
+
+        const datos = await respuesta.json();
+
+        if (datos.encontrado) {
+            nombre.textContent = datos.nombre;
+        } else {
+            nombre.textContent = "Participante no encontrado";
+        }
+
+    } catch (error) {
+        console.error(error);
+        nombre.textContent = "Error de conexión";
+    }
+
 });
