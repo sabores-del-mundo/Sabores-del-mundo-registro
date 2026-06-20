4a7a34ac4c0a9b45c821a1f1fdba384847bdbca9
sabores-del-mundo <congreso775@gmail.com>
2026-06-19 19:43:55 -0600

    Refactor script.js to simplify event listener
    Removed the fetch request and related error handling from the DOMContentLoaded event listener.

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     const nombre = document.getElementById("nombre");
 
-    const matriculaPrueba = "126381237";
-
-    try {
-
-        const respuesta = await fetch(
-            "https://script.google.com/macros/s/AKfycbwcQDGyo0rVFOd1Yt9fYQhwHMOpidgjKP2j67gNR3lbvyv-gCALKrhrxBgDTK214gn6_g/exec",
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    matricula: matriculaPrueba
-                })
-            }
-        );
-
-        const datos = await respuesta.json();
-        console.log(datos);
-        if (datos.encontrado) {
-            nombre.textContent = datos.nombre;
-        } else {
-            nombre.textContent = "Participante no encontrado";
-        }
-
-    } catch (error) {
-        console.error(error);
-        nombre.textContent = "Error de conexión";
-    }
+    nombre.textContent = "Conexión lista";
 
 });
